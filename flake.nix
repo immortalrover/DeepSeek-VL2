ef820a60594df4ba58d87093673c8ca599a1d7bc
immortalrover <immortalrover@protonmail.com>
2025-03-24 21:25:22 +0800

    bad

--- flake.nix
+++ flake.nix
@@ -5,27 +5,30 @@
     # It is unrelated to the URL of nixpkgs, any version can be used.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs }:
 
-  let
-    pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
-  in
-  {
-    devShells."x86_64-linux".default = pkgs.mkShell {
-      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-        pkgs.stdenv.cc.cc
-        "/run/opengl-driver"
-      ];
-      venvDir = ".venv";
-      packages = with pkgs; [
-        python312
-        # Do not install PyTorch with cuda directly here!!!
-        python312Packages.venvShellHook
-        python312Packages.pip
-        python312Packages.numpy
-        python312Packages.pyqt6
-        python312Packages.pysocks
-      ];
+  outputs =
+    { self, nixpkgs }:
+
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc
+          "/run/opengl-driver"
+        ];
+        venvDir = ".venv";
+        packages = with pkgs; [
+          python312
+          # Do not install PyTorch with cuda directly here!!!
+          python312Packages.venvShellHook
+          python312Packages.pip
+          python312Packages.pysocks
+        ];
+      };
     };
-  };
 }
